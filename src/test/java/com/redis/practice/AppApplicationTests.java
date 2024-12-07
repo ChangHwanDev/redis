2b4c4628a53305eb2ed8195313737dc77d5336eb
ChangHwanDev <wjdckdcjd@gmail.com>
2024-12-07 19:44:16 +0900

    test: Redis 락 동시성 테스트 코드 작성

---
+++ src/test/java/com/redis/practice/AppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.redis.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
